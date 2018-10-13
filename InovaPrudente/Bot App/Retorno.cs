cbd3686f6f610a9a78246a8069c1ea66857da101
murilocarrion <murilocarrion@hotmail.com>
2018-10-13 14:45:39 -0300

    botapp

---
+++ InovaPrudente/Bot App/Retorno.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bot_App
+{
+    public class Retorno
+    {
+        public int id { get; set; }
+        public string mensagem { get; set; }
+        
+        public Retorno()
+        {
+
+        }
+        public Retorno(int Id, string Mensagem)
+        {
+            this.id = Id;
+            this.mensagem = Mensagem;
+        }
+    }
+}
