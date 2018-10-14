0c4f5721f579b050e15840a290c9accd0f87c213
Victor Hugo Bertolino <vihbertolino@gmail.com>
2018-10-14 10:18:52 -0300

    Mensageria

---
+++ InovaPrudente/ServicoDeRecebimentoDeEntregas/RabbitMQ/Mensagem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicoDeRecebimentoDeEntregas.RabbitMQ
+{
+    public class Mensagem
+    {
+        public Entrega Entrega { get; set; } 
+        
+    }
+
+    public class Entrega
+    {
+        public int Id_Entrega { get; set; }
+        public string Nome_Cliente { get; set; }
+        public int IdEndereco { get; set; }
+        public DateTime DataPrevista { get; set; }
+        public int IdTipoEntrega { get; set; }
+        public int IdEmpresa { get; set; }
+        public string CodigoRastreio { get; set; }
+        public string Status { get; set; }
+        public string Logradouro { get; set; }
+    }
+}
