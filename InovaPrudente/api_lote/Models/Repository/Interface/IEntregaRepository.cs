b410778958f1476fb5f01db1cf457cfddaf322ea
Victor Hugo Bertolino <vihbertolino@gmail.com>
2018-10-14 10:06:26 -0300

    victor4

--- InovaPrudente/api_lote/Models/Repository/Interface/IEntregaRepository.cs
+++ InovaPrudente/api_lote/Models/Repository/Interface/IEntregaRepository.cs
@@ -10,6 +10,7 @@ namespace apiInovaPP.Models.Repository.Interface
     {
         Entrega.Entrega Get(int idEntrega);
         Entrega.Entrega Get(string codigoRastreio);
+        bool ExtenderPrazoColeta(Entrega.Entrega entrega);
         string GetCalcularTrocaEndereco(int idCidade, decimal distancia);
         bool PostTrocarEnderecoEntrega(Entrega.Entrega entrega, int idCidade, int distancia, string Logradouro2);
         bool Add(Entrega.Entrega entrega);
